b12460cc0fb813df3de0a3c724b6979b535e8a97
lilartus <lilit.mardanyan@tumo.org>
2017-12-25 17:38:55 +0400

    Lesson 4

---
+++ public/project3/js/pop_by_age.js
@@ -0,0 +1,26 @@
+$.getJSON("data/populations.json", function(jsonData){
+	var colData = [];
+	
+ // TODO
+  // populate colData
+
+  for(var i=0; i<jsonData.USA.length; i++){
+
+  	var myArray = [jsonData.USA[i]["age"], jsonData.USA[i]["value"]]
+  	colData.push(myArray)
+  }
+	
+
+
+	chart = c3.generate({
+		bindto: "#population-chart",
+		size: {
+			height: 450
+		},	
+		data: {
+			columns: colData, 
+			type : 'pie'
+
+		}
+	});	
+});
